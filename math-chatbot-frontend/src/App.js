8fdbcacb8532cb6b517f40f9c95a707d1e25bd4c
Kamalika Murugesan <kamalika2310617@ssn.edu.in>
2026-02-10 20:51:48 +0530

    Initialize App component with MathChatbot

---
+++ math-chatbot-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MathChatbot from './MathChatbot';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MathChatbot />
+    </div>
+  );
+}
+
+export default App;
